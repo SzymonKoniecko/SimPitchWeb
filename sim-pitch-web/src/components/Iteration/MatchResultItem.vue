700b7897bf5388dac19352cb1988034e888e00d4
Szymon Koniecko <s-koniecko@wp.pl>
2025-11-23 23:09:28 +0100

    league rounds in MatchResultItem

--- sim-pitch-web/src/components/Iteration/MatchResultItem.vue
+++ sim-pitch-web/src/components/Iteration/MatchResultItem.vue
@@ -1,27 +1,32 @@
 <template>
   <table class="match-body" selenium-id="match">
+    <h3>{{ getRoundNameById(matchRound.roundId) }}</h3>
     <colgroup>
-      <col>
-      <col>
-      <col>
+      <col />
+      <col />
+      <col />
     </colgroup>
     <tbody>
       <tr>
-        <td rowspan="5" class="team" :id="`round:${matchRound.roundId}^^team:${matchRound.homeTeamId}`">
+        <td
+          rowspan="5"
+          class="team"
+          :id="`round:${matchRound.roundId}^^team:${matchRound.homeTeamId}`"
+        >
           <TeamCard :id="matchRound.homeTeamId" variant="normal" />
         </td>
-        <td class="match-status"
+        <td
+          class="match-status"
           :class="{ played: matchRound.isPlayed, draw: matchRound.isDraw }"
         >
           {{
-            matchRound.isPlayed
-              ? matchRound.isDraw
-                ? "Draw"
-                : ""
-              : "Upcoming"
+            matchRound.isPlayed ? (matchRound.isDraw ? "Draw" : "") : "Upcoming"
           }}
         </td>
-        <td rowspan="5" :id="`round:${matchRound.roundId}^^team:${matchRound.awayTeamId}`">
+        <td
+          rowspan="5"
+          :id="`round:${matchRound.roundId}^^team:${matchRound.awayTeamId}`"
+        >
           <TeamCard :id="matchRound.awayTeamId" variant="normal" />
         </td>
       </tr>
@@ -55,7 +60,6 @@
   <hr />
 </template>
 
-
 <script setup lang="ts">
 import { computed } from "vue";
 import TeamCard from "../Team/TeamCard.vue";
@@ -63,12 +67,14 @@ import TeamStrengthItem from "../Team/TeamStrengthItem.vue";
 import type { MatchRound } from "../../models/SportsDataModels/matchRound";
 import type { Team } from "../../models/SportsDataModels/team";
 import type { TeamStrength } from "../../models/Iterations/teamStrength";
+import type { LeagueRound } from "../../models/SportsDataModels/leagueRound";
 
 type Props = {
   matchRound: MatchRound;
   teams: Team[];
   homeTeamStrength?: TeamStrength | null;
   awayTeamStrength?: TeamStrength | null;
+  leagueRounds: LeagueRound[];
 };
 
 const props = defineProps<Props>();
@@ -79,27 +85,33 @@ const awayTeamStrength = computed(() => props.awayTeamStrength);
 const getStadiumNameByTeamId = (id: string) => {
   const stadiumName = props.teams.find((t) => t.id === id)?.stadium?.name;
   return stadiumName ? `${stadiumName}` : "Stadium";
+};
+function getRoundNameById(id?: string): string {
+  if (id === null) {
+    return "Start";
+  }
+  return `Round of ${props.leagueRounds.find((lr) => lr.id === id)?.round}`;
 }
-  
 </script>
 
 <style scoped>
 table {
-    border-collapse: collapse;
-    border: none;
-    width: 100%;
-  }
-  td, th {
-    border: none;
-    padding: 8px;
-  }
-  col:nth-child(1),
-  col:nth-child(3) {
-    width: 40%;
-  }
-  col:nth-child(2) {
-    width: 20%;
-  }
+  border-collapse: collapse;
+  border: none;
+  width: 100%;
+}
+td,
+th {
+  border: none;
+  padding: 8px;
+}
+col:nth-child(1),
+col:nth-child(3) {
+  width: 40%;
+}
+col:nth-child(2) {
+  width: 20%;
+}
 .match-status {
   font-weight: 600;
   text-transform: uppercase;
@@ -112,16 +124,16 @@ table {
 }
 
 .match-body {
-    grid-template-columns: 1fr;
-    gap: 1.5rem;
-  }
+  grid-template-columns: 1fr;
+  gap: 1.5rem;
+}
 
 .team {
   width: 40%;
   max-width: 40%;
 }
 .team:hover,
-.score:hover{
+.score:hover {
   transform: scale(1.05);
 }
 .score {
@@ -134,7 +146,7 @@ table {
   min-width: 6rem;
 }
 .home-goals,
-.away-goals{
+.away-goals {
   font-size: large;
 }
 .separator {
