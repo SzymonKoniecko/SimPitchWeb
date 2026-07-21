20f29af71f02bc43e01bec006eab59074d5ddb83
Szymon Koniecko <s-koniecko@wp.pl>
2026-07-21 21:45:54 +0200

    #123 [M03] Fix: Restore simulations-nav selenium id

--- sim-pitch-web/src/components/Main/NavBar.vue
+++ sim-pitch-web/src/components/Main/NavBar.vue
@@ -27,7 +27,7 @@
         <router-link
           to="/simulation"
           exact-active-class="active"
-          selenium-id="simulations-nav-broken"
+          selenium-id="simulations-nav"
           >All simulations</router-link
         >
       </div>
