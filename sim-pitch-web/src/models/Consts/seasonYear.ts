61958a0c0351aa9461cc90c924cca96689e0fc34
Szymon Koniecko <s-koniecko@wp.pl>
2026-08-20 00:35:21 +0200

    update newest season year

--- sim-pitch-web/src/models/Consts/seasonYear.ts
+++ sim-pitch-web/src/models/Consts/seasonYear.ts
@@ -3,9 +3,10 @@ export const SeasonYear = {
   S2023_2024: '2023/2024',
   S2024_2025: '2024/2025',
   S2025_2026: '2025/2026',
+  S2026_2027: '2026/2027',
 } as const;
 
 export type SeasonYear = typeof SeasonYear[keyof typeof SeasonYear];
 
 export const seasonYearsOptions: SeasonYear[] = Object.values(SeasonYear);
-export const CURRENT_SEASON = SeasonYear.S2025_2026;
+export const CURRENT_SEASON = SeasonYear.S2026_2027;
